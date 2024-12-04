1c10d521ad57960fb4f09aef19c848821a154a9b
Andhikatp30 <aufarputra14@gmail.com>
2024-12-04 20:04:09 +0700

    3

--- resources/views/portofolio/portofolio.blade.php
+++ resources/views/portofolio/portofolio.blade.php
@@ -45,7 +45,7 @@
                             </div>
                         </div>
                     @endforeach
-                </div>
+                </div> 
             @endforeach
         </div>
     </div>
